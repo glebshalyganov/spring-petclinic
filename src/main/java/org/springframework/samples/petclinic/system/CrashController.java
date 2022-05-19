d9a6078a76f8764abb910390486ba813142bf4a1
shalyganov <g.shalyganov@haulmont.com>
2022-05-19 11:22:57 +0400

    kafka example

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -15,6 +15,8 @@
  */
 package org.springframework.samples.petclinic.system;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -28,10 +30,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 class CrashController {
 
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
 	@GetMapping("/oups")
 	public String triggerException() {
+		kafkaTemplate.send("test", "spring message");
 		throw new RuntimeException(
-				"Expected: controller used to showcase what " + "happens when an exception is thrown");
+			"Expected: controller used to showcase what " + "happens when an exception is thrown");
 	}
 
 }
